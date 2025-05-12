3eb9f695ef8a5ed7a603555236e09f470c895a69
MedhaviRajput <137051216+MedhaviRajput@users.noreply.github.com>
2025-05-12 16:22:48 +0530

    Create Subset_Sortstring_Repeat_Char_Recursion.cpp

---
+++ Subset_Sortstring_Repeat_Char_Recursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void subset(string str,string str1,int i){
+    if(i==str.length()){
+        cout<<"{"<<str1<<"}"<<endl;
+        return ;
+    }
+    str1+=str[i];
+    subset(str,str1,i+1);
+    str1.pop_back();
+    int in=i+1;
+    while(str[in]==str[in-1] && in<=str.length())
+      in=in+1;
+    subset(str,str1,in);
+    
+}
+int main(){
+    string str;
+    cin>>str;
+     cout<<"Real String ="<<str<<endl;
+    cout<<"Subsets of String :"<<endl;
+    subset(str,"",0);
+}
